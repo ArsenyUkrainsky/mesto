7320f5b2fe752692a0948380fefacffb170285a1
Arseniy <ArseniyUkrainskiy@yandex.ru>
2021-02-25 22:56:41 +0300

    fix: _review

--- scripts/Index.js
+++ scripts/Index.js
@@ -1,8 +1,8 @@
 //***Окно попап */
 // Переменная для выбора кнопки редактирования
-let PopupButtonEdit = document.querySelector('.profile__button-edit');
+let popupButtonEdit = document.querySelector('.profile__button-edit');
 // Переменная для выбора кнопки закрытия окна
-let PopupButtonClose = document.querySelector('.popup__close');
+let popupButtonClose = document.querySelector('.popup__close');
 // Переменная для окна попап
 let popup = document.querySelector('.popup');
 // Переменная для отключения скролла страницы во время просмотра окна
@@ -10,23 +10,23 @@ let scrollsw = document.querySelector('.root');
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__field_name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__field_characteristic'); // Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('.popup__field_input_name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__field_input_characteristic'); // Воспользуйтесь инструментом .querySelector()
 // Выберите элементы, куда должны быть вставлены значения полей
-let NameInfo = document.querySelector('.profile__info-name');
+let nameInfo = document.querySelector('.profile__info-name');
 let jobCharacteristic = document.querySelector('.profile__characteristic');
 
 // Функция добавления, удаления классов и скролла страницы, получение исходных данных текстовых полей
 function showPopup() {
   popup.classList.toggle('popup_opened');
   scrollsw.classList.toggle('root_scroll');
-  nameInput.value = NameInfo.textContent
+  nameInput.value = nameInfo.textContent
   jobInput.value = jobCharacteristic.textContent
 }
-/* function hidePopup() {
-     popup.classList.remove('popup_opened')
+function hidePopup() {
+     popup.classList.toggle('popup_opened')
      scrollsw.classList.toggle('root_scroll')
-} */
+}
 // Функция
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -36,15 +36,15 @@ function formSubmitHandler(evt) {
 
   // Получите значение полей jobInput и nameInput из свойства value
   // Вставьте новые значения с помощью textContent
-  NameInfo.textContent = nameInput.value
+  nameInfo.textContent = nameInput.value
   // console.log(NameInfo.textContent)
   jobCharacteristic.textContent = jobInput.value
-  showPopup()
+  hidePopup()
 }
 // Прикрепляем обработчик к кнопкам:
 // он будет следить за событием “click” - «нажатие»
-PopupButtonEdit.addEventListener('click', showPopup);
-PopupButtonClose.addEventListener('click', showPopup);
+popupButtonEdit.addEventListener('click', showPopup);
+popupButtonClose.addEventListener('click', hidePopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
