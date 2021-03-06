eae0a2b1681dfbb9c86f1a212a0359b8043bab90
Arseniy <ArseniyUkrainskiy@yandex.ru>
2021-03-06 18:07:12 +0300

    сделать рендер новой карточки

--- script/index.js
+++ script/index.js
@@ -1,20 +1,3 @@
-const popupButtonEdit = document.querySelector('.profile__button-edit') // Переменная для выбора кнопки редактирования
-const popupButtonAdd = document.querySelector('.profile__button-add') // Переменная для выбора кнопки добавления
-const popupButtonCloseProfile = document.querySelector('#closeProfile') // Переменная для выбора кнопки закрытия окна
-const popupButtonCloseCards = document.querySelector('#closeCards') // Переменная для выбора кнопки закрытия окна
-const popupUser = document.querySelector('#user') // Переменная для окна попап профиля
-const popupCards = document.querySelector('#cards') // Переменная для окна попап cards
-const scrollsw = document.querySelector('.root') // Переменная для переключения скролла страницы во время просмотра окна
-const formElement = document.querySelector('.popup__form') // Воспользуйтесь методом querySelector() Элемент формы в DOM
-const nameInfo = document.querySelector('.profile__info-name') // Выберите элементы, куда должны быть вставлены значения полей
-const jobCharacteristic = document.querySelector('.profile__characteristic') // Выберите элементы, куда должны быть вставлены значения полей
-const nameInput = formElement.querySelector('.popup__field_input_name') // Воспользуйтесь инструментом .querySelector() Из формы выбираем поле ввода имени
-const jobInput = formElement.querySelector('.popup__field_input_characteristic') // Воспользуйтесь инструментом .querySelector() Из формы выбираем поле ввода профессии
-
-
-
-
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -43,32 +26,61 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
   },
 ]
-// переменная куда вставлять результат
+
+const popupButtonEdit = document.querySelector('.profile__button-edit') // Переменная для выбора кнопки редактирования
+const popupButtonAdd = document.querySelector('.profile__button-add') // Переменная для выбора кнопки добавления
+const popupButtonCloseProfile = document.querySelector('#closeProfile') // Переменная для выбора кнопки закрытия окна
+const popupButtonCloseCards = document.querySelector('#closeCards') // Переменная для выбора кнопки закрытия окна
+const popupUser = document.querySelector('#user') // Переменная для окна попап профиля
+const popupCards = document.querySelector('#cards') // Переменная для окна попап cards
+const scrollsw = document.querySelector('.root') // Переменная для переключения скролла страницы во время просмотра окна
+
+const nameInfo = document.querySelector('.profile__info-name') // Выберите элементы, куда должны быть вставлены значения полей
+const jobCharacteristic = document.querySelector('.profile__characteristic') // Выберите элементы, куда должны быть вставлены значения полей
+
+const formElement = document.querySelector('.popup__form') // Воспользуйтесь методом querySelector() Элемент формы в DOM
+const nameInput = formElement.querySelector('.popup__field_input_name') // Воспользуйтесь инструментом .querySelector() Из формы выбираем поле ввода имени
+const jobInput = formElement.querySelector('.popup__field_input_characteristic') // Воспользуйтесь инструментом .querySelector() Из формы выбираем поле ввода профессии
 const container = document.querySelector('.elements__places')
-function createCard(item) {
-  return `
-  <li class="element">
-    <img src=${item.link} alt=${item.name} class="element__image" />
-    <div class="element__group">
-      <h2 class="element__title">${item.name}</h2>
-      <button type="button" class="element__like"></button>
-    </div>
-  </li>
-  `
+const templateElement = document.querySelector('.template')
+
+
+// console.log(templateElement)
+
+
+function createCardDomNode(item) {
+  
+  const newItem = templateElement.content.cloneNode(true)
+  const title = newItem.querySelector('.element__title')
+  const elementImage = newItem.querySelector('.element__image')
+  title.textContent = item.name
+  elementImage.setAttribute('alt', item.name)
+  elementImage.setAttribute('src', item.link)
+  // console.log(elementImage)
+  
+  
+
+  return newItem
 }
-// перебор массива
-// на выходе нужен измененный массив
-// результирующий массив - строки
-// нужно отрендерить массив, на каждом объекте шаблонная строка
-function renderList() {
-  const result = initialCards.map(createCard).join('')
-  //  превращает строку в разметку
-  container.insertAdjacentHTML('afterbegin', result)
+// Рендер всего списка карточек
+function renderCards() {
+  const result = initialCards.map(createCardDomNode)
+  container.append(...result) 
 }
+// Рендер одной новой карточки
+function addCardListener(evt) {
+  evt.preventDefault()
 
-renderList()
+  const inputCardName = formElement.querySelector('.popup__field_input_place')
+  const inputCardNameValue = inputCardName.value
+  const inputCardUrl = formElement.querySelector('.popup__field_input_url')
+  const inputCardUrlValue = inputCardUrl.value
 
+  const newCard = createCardDomNode({name: inputCardNameValue})
+  container.prepend(newCard)
 
+}
+renderCards()
 
 const togglePopupWindow = (popup) => {
   popup.classList.toggle('popup_opened')
@@ -99,19 +111,5 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler)
-
-
-function addCardListener(evt) {
-  evt.preventDefault()
-  const inputCardName = formElement.querySelector('.popup__field_input_place')
-  const inputCardUrl = formElement.querySelector('.popup__field_input_url')
-  const inputTitle = inputCardName.value
-  const inputUrl = inputCardUrl.value
-
-  const newCardName = createCard({name: inputTitle})
-  container.insertAdjacentHTML('afterbegin', newCardName)
-
-  inputCardName.value = ''
-}
 
 popupButtonAdd.addEventListener('submit', addCardListener)
